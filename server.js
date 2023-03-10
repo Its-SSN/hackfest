dd1cd81e56921eb8ba70bb82606c9f9886e7f5a9
SamudraMitra <94888874+SamudraMitra@users.noreply.github.com>
2023-03-11 00:12:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const DATABASE_URL = "mongodb+srv://cluster0.a2pgynk.mongodb.net/HackFest23"
+const DATABASE_URL =
+  "mongodb+srv://shreyanshandilya:NcjtCDV5by816iMH@cluster0.a2pgynk.mongodb.net/HackFest23";
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +14,7 @@ mongoose
   })
   .then(() => console.log("database connected successfully"))
   .catch((err) => console.log("error connecting to mongodb" + err));
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}...`);
 });
