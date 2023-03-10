09439286820d832b1a108056a1274f7e153c5f84
SamudraMitra <94888874+SamudraMitra@users.noreply.github.com>
2023-03-10 23:19:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 dotenv.config();
-const { readdirSync } = require("fs");
 const app = express();
 app.use(express.json());
-app.use(cors());
-readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r))); // remeber this syntax for creating routes dynamically
 app.use(bodyParser.urlencoded({ extended: true }));
 //database
 mongoose
@@ -21,4 +18,3 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}...`);
 });
-// console.log((+new Date() * Math.random()).toString().substring(0, 1));
