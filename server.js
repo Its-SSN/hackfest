718609d9d6caddbade7f423b0a3c05f7a644843d
Saumya Subhra Nandi <nandi.saumya2004@gmail.com>
2023-03-12 15:35:49 +0530

    authentication done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const DATABASE_URL = "mongodb+srv://shreyanshandilya:NcjtCDV5by816iMH@cluster0.a2pgynk.mongodb.net/HackFest23"
+// const DATABASE_URL = "mongodb+srv://shreyanshandilya:NcjtCDV5by816iMH@cluster0.a2pgynk.mongodb.net/HackFest23"
+const DATABASE_URL ="mongodb://localhost:27017/hackfest23"
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
